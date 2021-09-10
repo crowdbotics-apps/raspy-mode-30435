237f82cffb87ee9c357b0d5bd848ee47d7b79d68
Crowdbotics <team@crowdbotics.com>
2021-09-10 12:57:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "raspy_mode_30435Identifier",
+  url: "https://raspy_mode_30435Identifier.botics.co",
+  api: "https://raspy_mode_30435Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
